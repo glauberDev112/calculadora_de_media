7a74c80982a1a828fce342377c963ccb6dc1d5b5
glauber castro miranda <glauber.happy@gmail.com>
2022-11-11 12:43:40 -0300

    "files"

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const form = document.getElementById('atividade')
 const tabela = document.querySelector('tbody')
+const nm = parseInt(prompt('declare uma nota minima de aprovação (entre 1 e 10)'))
+const f = !((!isNaN(nm)) && (nm >= 1 && nm <= 10))
+console.log(`nota minima: ${nm} esta entre 1-10`)
+if (f) {
+    alert('erro:"o numero deve ser inteiro e estar entre 1 e 10"')
+    location.reload()
+}
 let notas = []
 console.log(form)
 form.addEventListener('submit',function (e) {
@@ -10,14 +17,23 @@ form.addEventListener('submit',function (e) {
 function add_linha () {
     const input_nome = document.getElementById('input-nome').value
     const input_nota = document.getElementById('input-nota').value
-    const status = input_nota >= 7 ? "aprovado" : "reprovado"
-    const msg_alt = input_nota >= 7 ? `o exercicio ${input_nome}, foi ${status}, parabaramens.` : `o exercicio ${input_nome}, foi ${status} por ${10-input_nota} pontos.`
+    let st = input_nota >= nm ? "aprovado" : "reprovado"
+    let icon = ''
+    if (input_nota == 10) {
+        icon = 'estrela.webp'
+    } else if (input_nota >= nm) {
+        icon = 'aprovado.png'
+    } else {
+        icon = 'reprovado.png'
+    }
+    const msg_alt = input_nota >= nm ? `o exercicio ${input_nome}, foi ${st}, parabaramens.` : `o exercicio ${input_nome}, foi ${status} por ${10-input_nota} pontos.`
     const nota_m = document.getElementById("nota-media")
     let linha = tabela.innerHTML
     linha += `<tr>
     <td>${input_nome}</td>
     <td>${input_nota}</td>
-    <td><img src="images/${status}.png" alt="${msg_alt}"></td>
+    <td>${100/10*input_nota}%</td>
+    <td><img src="images/${icon}" alt="${msg_alt}"></td>
 </tr><br>`
     notas.push(parseInt(input_nota))
     tabela.innerHTML = linha
@@ -25,12 +41,27 @@ function add_linha () {
 function mediar () {
     let sum = 0
     
-    for (let c = 0;c > notas.length; c++) {
-        sum = sum + notas[c]
+    for (let c = 0;c < notas.length; c++) {
+        console.log(`${sum} ${notas} ${notas[c]}`)
+        sum += notas[c]
+        
     } 
     let med = sum / notas.length
-    console.clear()
-    console.log(notas)
-    console.log(med)
-    console.log(sum)
+    const status = med >= nm
+    const pre_span = document.getElementById('por')
+    pre_span.innerHTML = 100/(notas.length*10)*sum
+    const status_ret = document.getElementById('resultado')
+    const nota_media = document.getElementById('nota-media')
+    nota_media.innerHTML = med
+    if (med == 10) {
+        status_ret.className = 'perfeito'
+        status_ret.innerHTML = 'aprovado!!!'
+    } else if (med >= nm) {
+        status_ret.className = 'aprovado'
+        status_ret.innerHTML = 'aprovado!!'
+        
+    } else {
+        status_ret.className = 'reprovado'
+        status_ret.innerHTML = 'reprovado!'
+    }
 }
